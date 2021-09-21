9f860fcdad9d736923353b4753a69e6df57d55d8
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-21 15:30:43 +1000

    couple more tests added

--- tests/testthat/test-aad.R
+++ tests/testthat/test-aad.R
@@ -1,3 +1,15 @@
 test_that("aad errors on non-numeric input", {
   expect_error(aad(x = "c"))
+  expect_error(aad(x = c(list(1))))
 })
+
+test_that("aad respects na.rm", {
+  expect_equal(aad(c(1, 3, NA), na.rm = FALSE), NA_real_)
+  expect_equal(aad(c(1, 3, NA), na.rm = TRUE), 1)
+
+  expect_error(aad(c(1, 3, NA), na.rm = NA))
+  expect_error(aad(c(1, 3, NA), na.rm = NULL))
+  expect_error(aad(c(1, 3, NA), na.rm = list(TRUE)))
+  expect_error(aad(c(1, 3, NA), na.rm = c(TRUE, FALSE)))
+})
+
