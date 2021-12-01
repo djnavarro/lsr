620053dc9d235868b6420f326970298d06bbda12
Danielle Navarro <d.navarro@unsw.edu.au>
2021-12-01 13:35:59 +1100

    moves maxFreq into modeOf.R

--- R/modeOf.R
+++ R/modeOf.R
@@ -91,3 +91,30 @@ modeOf <- function(x, na.rm = TRUE) {
   return( modal.values )
 
 }
+
+
+# maxFreq() returns the frequency of the sample mode.
+
+#' @rdname mode
+#' @export
+maxFreq <- function(x, na.rm = TRUE) {
+
+  if( !is.vector(x) & !is.factor(x) ) {
+    stop( '"x" must be a vector or a factor')
+  }
+  if( !methods::is(na.rm,"logical") | length(na.rm) !=1 ) {
+    stop( '"na.rm" must be a single logical value')
+  }
+
+  na.freq <- 0
+  if (na.rm == FALSE) { na.freq <- sum( is.na(x) ) }  # count the NAs if needed
+  x <- x[!is.na(x)]                                   # delete NAs
+  obs.val <- unique(x)                                # find unique values
+  valFreq <- function(x, y){ sum(y == x) }
+  freq <- unlist((lapply( obs.val, valFreq, x )))     # apply for all unique values
+  max.freq <- max(freq, na.freq)                      # modal frequency
+  return( max.freq )
+
+}
+
+
