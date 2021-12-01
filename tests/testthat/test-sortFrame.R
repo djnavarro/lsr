ecf4a1c06cd31aab148e0cc463b513693c75fec4
Danielle Navarro <d.navarro@unsw.edu.au>
2021-12-01 14:57:54 +1100

    adds minimal sortFrame test

---
+++ tests/testthat/test-sortFrame.R
@@ -0,0 +1,18 @@
+
+dataset <- data.frame(
+  txt = c("bob","Clare","clare","bob","eve","eve"),
+  num1 = c(3,1,2,0,0,2),
+  num2 = c(1,1,3,0,3,2),
+  etc = c("not","used","as","a","sort","term"),
+  stringsAsFactors = FALSE
+)
+
+f1 <- sortFrame(dataset, num1)
+f2 <- sortFrame(dataset, num1, num2)
+f3 <- sortFrame(dataset, txt)
+
+test_that("sortFrame example works", {
+  expect_equal(f1$etc, c("a", "sort", "used", "as", "term", "not"))
+  expect_equal(f2$etc, c("a", "sort", "used", "term", "as", "not"))
+  expect_equal(f3$etc, c("not", "a", "as", "used", "sort", "term"))
+})
